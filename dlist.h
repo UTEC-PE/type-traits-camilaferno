6f4cafafe1c536bf2c68d8cfd5209f8392671c97
Camila Fernandez <camila.fernandez@utec.edu.pe>
2018-09-05 17:58:21 -0500

    dlist

--- dlist.h
+++ dlist.h
@@ -4,58 +4,114 @@
 #include "iterator.h"
 
 template <typename T>
-class DListIterator : public Iterator<T> {     
-    public: 
+class DListIterator : public Iterator<T> {
+    public:
         DListIterator() : Iterator<T>() {};
         DListIterator(Node<T> *current) : Iterator<T>(current) {};
-        DListIterator<T> operator++();
-        DListIterator<T> operator--();
+        DListIterator<T> operator++(){
+          this -> current = this -> current -> next;
+          return *this;
+        };
+        DListIterator<T> operator--(){
+          this -> current = this -> current -> prev;
+          return *this;
+        };
 };
 
 template <typename Tr>
-class DList {     
+class DList {
     public:
         typedef typename Tr::T T;
         typedef DListIterator<T> iterator;
-      
+
     private:
         Node<T>* head;
         Node<T>* tail;
-              
+
     public:
         DList() {
             head = tail = nullptr;
         };
 
         void push_front(T data) {
-            // TODO
+          Node<T>* tmp = new Node<T>(data);
+            if(!head){
+              tmp -> next = nullptr;
+              tmp -> prev = nullptr;
+              head=tail=tmp;
+            }
+            else{ //apuntar data next a nullptr?
+              head -> prev = tmp;
+              tmp -> next = head;
+              tmp -> prev = nullptr;
+              head=tmp;
+            }
         }
 
         void push_back(T data) {
-            // TODO
+          Node<T>* tmp = new Node<T>(data);
+            if(!head){ //da igual pq si esta vacio head y tail estan vacios
+              tmp -> next = nullptr;
+              tmp -> prev = nullptr;
+              head=tail=tmp;
+            }
+            else{
+              tail -> next = tmp;
+              tmp -> prev = tail;
+              tmp -> next = nullptr;
+              tail=tmp;
+            }
         }
-             
-        void pop_front() {
-            // TODO
+
+        void pop_front() { //puede estar mal pq lo primero que hago es mover a head
+            if(!head){
+              throw "Empty List";
+            }
+            else{
+              if(!head->next){
+                delete head;
+              }
+              Node<T>* tmp = head->next;
+              head -> next = nullptr;
+              head -> prev = nullptr;
+              tmp -> prev = nullptr;
+              delete head;
+              head=tmp;
+            }
         }
-             
+
         void pop_back() {
-            // TODO
+            if(!head){
+              throw "Empty List";
+            }
+            else{
+              if(!head->next){
+                delete head;
+              }
+              else{
+                Node<T>* tmp = tail->prev;
+                tail -> next = nullptr;
+                tail -> prev = nullptr;
+                tmp -> next = nullptr;
+                delete tail;
+                tail=tmp;
+              }
+            }
         }
-             
+
         iterator begin() {
-            // TODO
+          return iterator(head);
         }
-             
-        iterator end() {
-            // TODO
+
+        iterator end() {;
+          return iterator(tail);
         }
-             
+
         ~DList() {
             if (head) {
                 head->killSelf();
-            } 
-        }         
+            }
+        }
 };
 
 #endif
--- iterator.h
+++ iterator.h
@@ -7,14 +7,29 @@ template <typename T>
 class Iterator {
     protected:
         Node<T>* current;
-      
+
     public:
         Iterator() : current(nullptr) {};
         Iterator(Node<T>* current) : current(current) {};
-             
-        Iterator<T> operator=(Iterator<T> other);
-        bool operator!=(Iterator<T> other);
-        T operator*();
-};  
+
+        Iterator<T> operator=(Iterator<T> other){
+          current = other.current;
+
+          return *this;
+        };
+
+        bool operator!=(Iterator<T> other){
+          if(current!=other.current){
+            return true;
+          }
+          else{
+            return false;
+          }
+        };
+
+        T operator*(){
+          return current -> data;
+        };
+};
 
 #endif
